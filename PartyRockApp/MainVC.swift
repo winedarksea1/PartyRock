3a3955a6ace1f6e8b19adf7417011941f3e67215
Andrew McGovern <winedark.mcgovern@gmail.com>
2017-11-13 10:06:33 -0500

    first commit

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Andrew McGovern on 11/11/17.
+//  Copyright © 2017 Andrew McGovern. All rights reserved.
+//
+
+import UIKit
+// THIS IS A BASIC TEMPLATE OF EVERYTHING
+// A VIEW CONTROLLER NEEDS WHEN WORKING WITH
+// A TABLE VIEW
+class MainVC: UIViewController, UITableViewDelegate,
+    UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var partyRocks = [PartyRock]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let p1 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Lights Out")
+        
+        let p2 = PartyRock(imageURL: "http://www.croshalgroup.com/wp-content/uploads/2015/05/Redfoo-Website.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/1w9DiGlZksU\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Let's Get Ridiculous")
+        let p3 = PartyRock(imageURL: "https://i.ytimg.com/vi/2wUxw6GH3IM/hqdefault.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/vg_nvEGryA4\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Juicy Wiggle Lesson")
+        let p4 = PartyRock(imageURL: "http://www.billboard.com/files/styles/article_main_image/public/media/lmfao-party-rock-anthem-2011-billboard-650.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/gZIqW92GaTQ\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Party Rock Commercial")
+        let p5 = PartyRock(imageURL: "http://direct-ns.rhap.com/imageserver/v2/albums/Alb.219913217/images/600x600.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/tWyuglGCKgE\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Juicy Wiggle")
+        
+        
+        partyRocks.append(p1)
+        partyRocks.append(p2)
+        partyRocks.append(p3)
+        partyRocks.append(p4)
+        partyRocks.append(p5)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    
+    // The following two functions, at the very least, will have to be included in your
+    // ViewController when working with table views
+    
+    // This function tells Swift how to reuse cells
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // DON'T FORGET TO SET THE IDENTIFIER 
+        // ON THE TABLE VIEW CELL IN THE Main.storyboard
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PartyCell", for: indexPath) as? PartyCell {
+            let partyRock = partyRocks[indexPath.row]
+            cell.updateUI(partyRock: partyRock)
+            
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    // This tells Swift how many content objects you have in the array
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return partyRocks.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let partyRock = partyRocks[indexPath.row]
+        
+        performSegue(withIdentifier: "VideoVC", sender: partyRock)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let theDestination = segue.destination as? VideoVC {
+            if let party = sender as? PartyRock {
+                theDestination.partyRock = party
+            }
+        }
+    }
+
+}
+
---
+++ PartyRockApp/PartyCell.swift
@@ -0,0 +1,41 @@
+//
+//  PartyCell.swift
+//  PartyRockApp
+//
+//  Created by Andrew McGovern on 11/11/17.
+//  Copyright © 2017 Andrew McGovern. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    
+    @IBOutlet weak var videoTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func updateUI(partyRock: PartyRock) {
+        videoTitle.text = partyRock.videoTitle
+        
+        let url = URL(string: partyRock.imageURL)!
+        
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            } catch {
+                // handle the error
+            }
+                
+        }
+        
+    }
+
+}
